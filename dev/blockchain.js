a56b8d412e18340d12f9399a38f484518d901376
stevearchuleta <stevearchuleta@me.com>
2021-03-29 08:15:19 -0700

    rename the newTransactions array to pendingTransactions; write a createNewTransaction prototype method that takes in an amount, a sender, and a recipient; push this newTransaction into the pendingTransactions array; return the return the exact identifier of the block where this newTransaction will be located

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,7 +1,7 @@
 
 function Blockchain() {
     this.chain = [];            // the substantive blocks that are created and mined in my blockchain will be stored in this chain array.
-    this.newTransactions = [];  // new transactions are held in this newTransactions array.
+    this.pendingTransactions = [];  // purgatory: new transactions are held in this pendingTransactions array, not yet recorded onto the blockchain.
 }
 
 
@@ -9,14 +9,14 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {          // these newBlocks will be block(s) inside of the chain array.
         index : this.chain.length + 1,
         timestamp : Date.now(),
-        transactions : this.newTransactions,  // when a new block is created, all of the new/pending transactions will be stored in the newTransaction array.
+        transactions : this.pendingTransactions,  // when a new block is created (via mining), all of the new/pending transactions will be stored in the chain array.
         nonce : nonce,   // arbitrary number that is a generated proof of work.
         hash : hash,   // data from the new/current block yielded by a hash of the transaction property
         previousBlockHash : previousBlockHash  //data from the previous block instance.
     };
 
-    this.newTransactions = [];      // once all of the transactions have been placed into this new/current block and stored in the newTransactions array,
-                                    // clear the newTransactions array for another instance
+    this.pendingTransactions = [];      // once all of the transactions have been placed into this new/current block and stored in the pendingTransactions array,
+                                    // clear the pendingTransactions array for another instance
     this.chain.push(newBlock);
 
     return newBlock;
@@ -25,6 +25,19 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 
 Blockchain.prototype.getLastBlock = function() {
     return this.chain[this.chain.length -1];
+};
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount : amount,
+        sender : sender,
+        recipient : recipient
+    }
+    this.pendingTransactions.push(newTransaction);
+
+    // return the exact identifier of the block where this newTransaction will be located (the latest block in the chain array) by invoking this.GetLastBlock() (which returns the soon-to-be previous block object in the chain array)
+    // retrieve the index property (the identifier) of this particular block in the chain array; the plus one means that I am one-block-to-the-right of the previous-getLastBlock()-block in the chain array.
+    return this.getLastBlock()['index'] + 1;
 };
 
 module.exports = Blockchain;
