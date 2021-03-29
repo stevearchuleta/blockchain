bf432c52aa5ec39c9c65795b7429508497e7a3c5
stevearchuleta <stevearchuleta@me.com>
2021-03-28 18:28:26 -0700

    write a createNewBlock() method to create a object called newBlock that holds an index property (that references the chain array), a timestamp property, a transaction property (that will be hashed), a nonce property (proof of work identifier), a hash property, and the previousBlockHash property; push newBlock into the chain array; return newBlock

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,24 @@
 
 function Blockchain() {
-    this.chain = [];            // the substantive blocks that are created and mined in my blockchain will be stored in this chain array
-    this.newTransactions = [];  // new transactions are held in this newTransactions array
+    this.chain = [];            // the substantive blocks that are created and mined in my blockchain will be stored in this chain array.
+    this.newTransactions = [];  // new transactions are held in this newTransactions array.
 }
+
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
+    const newBlock = {          // these newBlocks will be block(s) inside of the chain array.
+        index : this.chain.length + 1,
+        timestamp : Date.now(),
+        transactions : this.newTransactions,  // when a new block is created, all of the new/pending transactions will be stored in the newTransaction array.
+        nonce : nonce,   // arbitrary number that is a generated proof of work.
+        hash : hash,   // data from the new/current block yielded by a hash of the transaction property
+        previousBlockHash : previousBlockHash  //data from the previous block instance.
+    };
+
+    this.newTransactions = [];      // once all of the transactions have been placed into this new/current block and stored in the newTransactions array,
+                                    // clear the newTransactions array for another instance
+    this.chain.push(this.newBlock);
+
+    return newBlock;
+
+};
