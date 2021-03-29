0e62d1d8aaea7baf696f421e002743fee8d32e0b
stevearchuleta <stevearchuleta@me.com>
2021-03-28 18:04:30 -0700

    write Blockchain constructor with a chain property and a newTransaction property, both initialized with empty arrays

---
+++ dev/blockchain.js
@@ -0,0 +1,5 @@
+
+function Blockchain() {
+    this.chain = [];            // the substantive blocks that are created and mined in my blockchain will be stored in this chain array
+    this.newTransactions = [];  // new transactions are held in this newTransactions array
+}
